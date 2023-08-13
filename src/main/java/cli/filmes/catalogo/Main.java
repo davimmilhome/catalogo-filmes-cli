4212bb6063b19fb9d624e6245bc9b6d51cf01116
davimmilhome <davimmilhome@gmail.com>
2023-08-13 07:45:12 -0300

    wip: iniciando o arquivo Main.java

--- src/main/java/cli/filmes/catalogo/Main.java
+++ src/main/java/cli/filmes/catalogo/Main.java
@@ -1,10 +1,11 @@
 package main.java.cli.filmes.catalogo;
 
+import main.java.cli.filmes.catalogo.ui.PagedListUI;
 import main.java.cli.filmes.catalogo.ui.TemplateUI;
 
 public class Main {
     public static void main (String[] args) {
-        TemplateUI ui = new TemplateUI("Catologo Filmes CLI");
+        TemplateUI ui = new PagedListUI("PaginaListada");
         ui.show();
     }
 }
