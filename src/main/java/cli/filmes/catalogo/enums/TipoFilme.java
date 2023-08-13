354c6ac70a4c4e66b5505c9363d7e09e9a6b25a2
Fikda <fabianaikeda@yahoo.com.br>
2023-08-13 11:22:41 -0300

    feat: criacao enum TipoFilme e classe Filme

---
+++ src/main/java/cli/filmes/catalogo/enums/TipoFilme.java
@@ -0,0 +1,5 @@
+package main.java.cli.filmes.catalogo.enums;
+
+public enum TipoFilme {
+    ACAO, COMEDIA, ROMANCE, SCIFI, DRAMA, POLICIAL, HISTORICO, SUSPENSE;
+}
---
+++ src/main/java/cli/filmes/catalogo/models/Filme.java
@@ -0,0 +1,95 @@
+package main.java.cli.filmes.catalogo.models;
+
+import main.java.cli.filmes.catalogo.enums.TipoFilme;
+
+import java.util.List;
+
+public class Filme {
+ TipoFilme tipoFilme;
+ protected String nomeFilme;
+ protected String dataLancamento;
+ protected String sinopse;
+ protected Diretor diretor;
+ protected List<Ator> ator;
+
+ protected int orcamento;
+
+    public Filme(TipoFilme tipoFilme, String nomeFilme, String dataLancamento, String sinopse, Diretor diretor, List<Ator> ator, int orcamento) {
+        this.tipoFilme = tipoFilme;
+        this.nomeFilme = nomeFilme;
+        this.dataLancamento = dataLancamento;
+        this.sinopse = sinopse;
+        this.diretor = diretor;
+        this.ator = ator;
+        this.orcamento = orcamento;
+    }
+
+    public TipoFilme getTipoFilme() {
+        return tipoFilme;
+    }
+
+    public void setTipoFilme(TipoFilme tipoFilme) {
+        this.tipoFilme = tipoFilme;
+    }
+
+    public String getNomeFilme() {
+        return nomeFilme;
+    }
+
+    public void setNomeFilme(String nomeFilme) {
+        this.nomeFilme = nomeFilme;
+    }
+
+    public String getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(String dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+    public Diretor getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(Diretor diretor) {
+        this.diretor = diretor;
+    }
+
+    public List<Ator> getAtor() {
+        return ator;
+    }
+
+    public void setAtor(List<Ator> ator) {
+        this.ator = ator;
+    }
+
+    public int getOrcamento() {
+        return orcamento;
+    }
+
+    public void setOrcamento(int orcamento) {
+        this.orcamento = orcamento;
+    }
+
+    @Override
+    public String 'git toString() {
+        return "Filme{" +
+                "tipoFilme=" + tipoFilme +
+                ", nomeFilme='" + nomeFilme + '\'' +
+                ", dataLancamento='" + dataLancamento + '\'' +
+                ", sinopse='" + sinopse + '\'' +
+                ", diretor=" + diretor +
+                ", ator=" + ator +
+                ", orcamento=" + orcamento +
+                '}';
+    }
+}
