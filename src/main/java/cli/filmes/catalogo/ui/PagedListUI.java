172933a34cddb50f28de3e7cb3a2269c30fea2ad
davimmilhome <davimmilhome@gmail.com>
2023-08-14 18:44:10 -0300

    wip: adicionando recebimento da lista páginada

--- src/main/java/cli/filmes/catalogo/ui/PagedListUI.java
+++ src/main/java/cli/filmes/catalogo/ui/PagedListUI.java
@@ -2,23 +2,34 @@ package main.java.cli.filmes.catalogo.ui;
 
 import main.java.cli.filmes.catalogo.utils.ConsoleUIHelper;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PagedListUI extends TemplateUI {
 
     protected final int PAGE_SIZE;
     protected int actualPage;
 
-    public PagedListUI(String title) {
-        this(DEFAULT_ROWS, DEFAULT_COLS, title);
+    protected PagedList pageSource;
+
+    public PagedListUI(String title, PagedList pageSource) {
+        this(DEFAULT_ROWS, DEFAULT_COLS, title, pageSource);
+        // **
     }
 
-    public PagedListUI(int linhas, int colunas, String title) {
+    public PagedListUI(int linhas, int colunas, String title, PagedList pageSource) {
         super(linhas, colunas, title);
         PAGE_SIZE = linhas - 4; // Cabeçalho 3 rows + linha separadora
         actualPage = 1;
+        this.pageSource = pageSource;
     }
 
     @Override
     public int drawContent() {
+        List dataList = pageSource**;
+        for (int i = 0; i < PAGE_SIZE; i++) {
+            dataList.get(i);
+        }
         return 0;
     };
     @Override
