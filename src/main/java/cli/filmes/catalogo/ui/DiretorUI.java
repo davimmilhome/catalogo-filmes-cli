7aec8b702e0e5d5329b90cad5103ecc50c242724
Bacs <bruno.acsl@gmail.com>
2023-08-16 19:07:44 -0300

    Feat: Adicionando DiretorUI

---
+++ src/main/java/cli/filmes/catalogo/ui/DiretorUI.java
@@ -0,0 +1,60 @@
+package main.java.cli.filmes.catalogo.ui;
+import main.java.cli.filmes.catalogo.models.Diretor;
+import main.java.cli.filmes.catalogo.utils.ConsoleUIHelper;
+
+import java.time.LocalDate;
+
+public class DiretorUI extends EditItemUI<Diretor>{
+
+    public DiretorUI(String title, Diretor item, EditItemCallback<Diretor> editItemCallback) {
+        super(title, item, editItemCallback);
+    }
+
+    @Override
+    public int drawItemDetails() {
+        ConsoleUIHelper.drawWithRightPadding("Nome: " +
+                item.getNome(), cols, ' ');
+        ConsoleUIHelper.drawWithRightPadding("Sobrenome: " + item.getSobrenome(), cols, ' ');
+        ConsoleUIHelper.drawWithRightPadding("Data de nascimento: " + item.getDob(), cols, ' ');
+        ConsoleUIHelper.drawWithRightPadding("Idade: " + item.getIdade(), cols, ' ');
+        ConsoleUIHelper.drawWithRightPadding("Nacionalidade: " + item.getNacionalidade(), cols, ' ');
+        return 5;
+    }
+
+    @Override
+    public String[] fillFieldsNames() {
+        return new String[]{"Nome", "Sobrenome", "Data de nascimento", "Idade", "Nacionalidade"};
+    }
+
+    @Override
+    public void fillField(Diretor item, int option) {
+        switch (option) {
+            case 0:
+                break;
+            case 1:
+                String nome = ConsoleUIHelper.askSimpleInput(
+                        "Informe o nome: ");
+                item.setNome(nome);
+                break;
+            case 2:
+                String sobrenome = ConsoleUIHelper.askSimpleInput("Informe o sobrenome: ");
+                item.setSobrenome(sobrenome);
+                break;
+            case 3:
+                LocalDate dob = LocalDate.parse(ConsoleUIHelper.askSimpleInput("Informe a data de nascimento: "));
+                item.setDob(dob);
+                break;
+            case 4:
+                String nacionalidade = ConsoleUIHelper.askSimpleInput("Informe a nacionalidade: ");
+                item.setNacionalidade(nacionalidade);
+                break;
+        }
+    }
+
+    @Override
+    protected void newItem() {
+        Diretor diretor = null;
+        DiretorUI ui = new DiretorUI(title + " :: Novo", null, editItemCallback);
+        ui.show();
+    }
+}
