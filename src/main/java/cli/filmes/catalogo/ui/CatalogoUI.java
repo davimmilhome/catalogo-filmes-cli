a42ecf5f0c0a08a413cb7a8308bf4ac5b74a675a
Junior Carvalho <junior.carvalho@educat.com.br>
2023-08-17 21:37:22 -0300

    Feat: Implementando o metodo isNew do showItem e o metodo addItem da classe CatalogoUI

--- src/main/java/cli/filmes/catalogo/ui/CatalogoUI.java
+++ src/main/java/cli/filmes/catalogo/ui/CatalogoUI.java
@@ -9,33 +9,53 @@ public class CatalogoUI extends PagedListUI<Filme> {
     }
 
 
-//    protected void showItem(Filme item) {
-//        FilmeUI ui = new FilmeUI("", item, new EditItemCallback<Filme>() {
-//            @Override
-//            public void remove(Filme item) {
-//
-//            }
-//
-//            @Override
-//            public void add(Filme item) {
-//
-//            }
-//
-//            @Override
-//            public boolean isNew(Filme item) {
-//                return false;
-//            }
-//        });
-//        ui.show();
-//    }
-
-
-    @Override
     protected void showItem(Filme item) {
+        FilmeUI ui = new FilmeUI("", item, new EditItemCallback<Filme>() {
+            @Override
+            public void remove(Filme item) {
 
+            }
+
+            @Override
+            public void add(Filme item) {
+
+            }
+
+            @Override
+            public boolean isNew(Filme ref) {
+                if (ref.equals(item)) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        });
+        ui.show();
     }
 
     protected void addItem() {
+        Filme f1 = new Filme("Teste");
+        FilmeUI ui = new FilmeUI("Novo Filme", f1, new EditItemCallback<>() {
+            @Override
+            public void remove(Filme item) {
+                System.out.println(item.getNomeFilme());
+            }
+
+            @Override
+            public void add(Filme item) {
+                System.out.println(item.getNomeFilme());
+            }
+
+            @Override
+            public boolean isNew(Filme ref) {
+                if (ref.equals(f1)) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        });
+        ui.show();
     }
 
 }
